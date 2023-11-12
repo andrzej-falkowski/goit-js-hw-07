a4ffd4431588cf0ee546ac6bde1aa46e713b1a34
andrzej-falkowski <138798724+andrzej-falkowski@users.noreply.github.com>
2023-11-12 23:34:44 +0100

    Added lightbox.html and lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('ul.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+function createGalleryMarkup(element) {
+  return element
+    .map(
+      ({ preview, original, description }) =>
+        `<li>
+<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</li>`
+    )
+    .join("");
+}
+
+gallery.insertAdjacentHTML("beforeend", galleryMarkup);
+
+const lightbox = new SimpleLightbox('ul.gallery a', {
+captionPosition:'bottom',
+captionDelay: 250,
+captionsData: 'alt'});
