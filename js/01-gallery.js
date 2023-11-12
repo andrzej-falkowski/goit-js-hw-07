55128764a5b6755a1229a343c7e7538660b1ee3b
andrzej-falkowski <138798724+andrzej-falkowski@users.noreply.github.com>
2023-11-12 22:10:53 +0100

    Added gallery.html and gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector("ul.gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+function createGalleryMarkup(element) {
+  return element
+
+    .map(
+      ({ preview, original, description }) =>
+        `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+    )
+    .join("");
+}
+
+gallery.addEventListener("click", (event) => {
+  event.preventDefault();
+  const eventTarget = event.target.dataset.source;
+  if (!eventTarget) return;
+
+  const lightboxInstance = basicLightbox.create(
+    `<img width="1280" src="${eventTarget}"/img>`
+  );
+  lightboxInstance.show();
+
+  function onEscape(event) {
+    if (event.code === "Escape") {
+      lightboxInstance.close();
+    }
+  }
+  document.addEventListener("keydown", onEscape);
+});
+
+gallery.insertAdjacentHTML("beforeend", galleryMarkup);
